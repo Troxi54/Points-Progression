5e0c23f9e01f17b5788f718f7d6d59ad5ee466e5
Troxi <111352139+Troxi54@users.noreply.github.com>
2026-04-12 03:17:31 +0500

    Technical improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: "./",
   server: {
     port: 5173,
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   build: {
     rollupOptions: {
